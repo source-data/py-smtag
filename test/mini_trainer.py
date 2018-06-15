8f7e0a18fee76e79b09b04fd65b1869a8c4bacd4
tlemberger <thomas.lemberger@gmail.com>
2018-06-15 23:12:35 +0200

    first test for engine

---
+++ test/mini_trainer.py
@@ -0,0 +1,38 @@
+from torch import nn, optim
+from smtag.builder import build
+from smtag.progress import progress
+
+def toy_model(x, y, selected_feature = ['geneprod'], threshold = 1E-02):
+        opt = {}
+        opt['namebase'] = 'test_importexport'
+        opt['learning_rate'] = 0.01
+        opt['epochs'] = 100
+        opt['minibatch_size'] = 1
+        opt['selected_features'] = selected_feature
+        opt['nf_table'] =  [8,8]
+        opt['pool_table'] = [2,2]
+        opt['kernel_table'] = [2,2]
+        opt['dropout'] = 0.1
+        opt['nf_input'] = x.size(1)
+        opt['nf_output'] =  y.size(1)
+        opt = opt
+        model = build(opt)
+
+        # we do the training loop here instead of using smtag.trainer to avoid the need to prepare minibatches
+        loss_fn = nn.SmoothL1Loss() # nn.BCELoss() # 
+        optimizer = optim.Adam(model.parameters(), lr = opt['learning_rate'])
+        optimizer.zero_grad()
+        loss = 1
+        i = 0
+        # We stop as soon as the model has reasonably converged or if we exceed a max number of iterations
+        max_iterations = opt['epochs']
+        while loss > threshold and i < max_iterations:
+            progress(i, max_iterations)
+            loss = loss_fn(model(x), y)
+            loss.backward()
+            optimizer.step()
+            i += 1
+        print("Model preparation done! {} iterations reached loss={}".format(i, float(loss)))
+        # don't forget to set the state of the model to eval() to avoid Dropout
+        model.eval()
+        return model
---
+++ test/test_engine.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+#T. Lemberger, 2018
+
+import unittest
+import torch
+from torch import nn, optim
+from smtag.utils import tokenize
+from smtag.converter import Converter
+from test.smtagunittest import SmtagTestCase
+from test.mini_trainer import toy_model
+from smtag.engine import SmtagEngine, Combine, Connector
+from smtag.progress import progress
+from smtag.config import MARKING_CHAR
+
+#maybe import https://github.com/pytorch/pytorch/blob/master/test/common.py and use TestCase()
+
+
+class EngineTest(SmtagTestCase):
+
+    @classmethod
+    def setUpClass(self): # run only once
+        self.text_example = "AAAAAAA XXX AAA"
+        self.x = Converter.t_encode(self.text_example)
+        self.y = torch.Tensor(# A A A A A A A   X X X   A A A 
+                             [[[0,0,0,0,0,0,0,0,1,1,1,0,0,0,0]]])
+        self.selected_features = ["geneprod"]
+        self.entity_model = toy_model(self.x, self.y)
+
+        self.anonymized_text_example = self.text_example.replace("X", MARKING_CHAR)
+        self.z = Converter.t_encode(self.anonymized_text_example)
+        self.context_model = toy_model(self.z, self.y, selected_feature=['intervention'])
+
+    def test_model_stability(self): 
+        '''
+        Testing that test model returns the same result
+        '''
+        iterations = 100
+        for i in range(iterations):
+            y_1 = self.entity_model(self.x)
+            self.assertTensorEqual(self.y, y_1)
+
+    def test_engine_entity(self):
+        e = SmtagEngine()
+        ml = e.entities('stained by anti‐SEPA‐1 antibodies')
+        print(ml)
+        expected = 'stained by anti‐<sd-tag type="geneprod">SEPA‐1</sd-tag> antibodies'
+        self.assertEqual(expected, ml)
+
+
+if __name__ == '__main__':
+    unittest.main()
