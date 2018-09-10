88e39209844e36ae7eb600f3203e111a6b127d26
tlemberger <thomas.lemberger@gmail.com>
2018-09-10 23:13:36 +0200

    add a Benchmark class

--- smtag/train/evaluator.py
+++ smtag/train/evaluator.py
@@ -3,9 +3,11 @@
 
 import torch
 from .minibatches import Minibatches
+from .loader import Loader
 from ..predict.binarize import Binarized
 from ..common.progress import progress
 from ..common.config import DEFAULT_THRESHOLD
+from ..common.importexport import load_model
 
 class Accuracy(object):
  
@@ -66,12 +68,12 @@ class Accuracy(object):
     def tpfp(prediction, target, threshold=0.99):
         """
         Computes accuracy at the level of individual characters.
-        
+
         Args:
             threshold (0..1): to call a character as tagged ('hit')
             prediction (4D tensor): the prediction for which the accuracy has to be computed
             target (4D tensor): the target to which the prediction has to be compared to 
-        
+
         Returns:
             Three 1D tensors with the number per feature of positives, true positives and false positives, respectively
         """
@@ -82,3 +84,28 @@ class Accuracy(object):
         fp = (predx - target).clamp(0).sum(2).sum(0) # false positives when prediction is 1 but target is 0
         tp = predx.sum(2).sum(0) - fp 
         return p.view(nf), tp.view(nf), fp.view(nf) # output as 1 dim tensor with one metric per output feature
+
+class Benchmark():
+
+    def __init__(self, model_basename, testset_basename, tokenize):
+        self.model = load_model(model_basename)
+        self.opt = self.model.opt
+        self.opt['validation_fraction'] = 0 # this will set the dataset mode into testset mode
+        self.tokenize = tokenize
+        loader = Loader(self.opt)
+        testset = loader.prepare_datasets(testset_basename)
+        testset = Minibatches(testset['test'], self.opt['minibatch_size'])
+        benchmark = Accuracy(self.model, testset, tokenize=self.tokenize)
+        self.precision, self.recall, self.f1 = benchmark.run()
+
+    def display(self):
+        print("\n Global stats: \27[1m")
+        print("\t\27[32;1mprecision\27[0m = {}.2f".format(self.precision.mean()))
+        print("\t\27[33;1mrecall\27[0m = {}.2f".format(self.recall.mean()))
+        print("\t\27[36;1mf1\27[0m = {}.2f".format(self.f1.mean()))
+
+        for i, feature in enumerate(self.model.output_semantics):
+                print("\n Feature: '\27[1m{}\27[0m'\n".format(feature))
+                print("\t\27[32;1mprecision\27[0m = {}.2f".format(self.precision[i]))
+                print("\t\27[33;1mrecall\27[0m = {}.2f".format(self.recall[i]))
+                print("\t\27[36;1mf1\27[0m = {}.2f".format(self.f1[i]))
