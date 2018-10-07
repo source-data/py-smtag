ecfbeecdaa28763c11b6faebdd510e09b87fc7c4
tlemberger <thomas.lemberger@gmail.com>
2018-10-07 13:06:37 +0200

    first working version of context by ocr

--- smtag/datagen/context.py
+++ smtag/datagen/context.py
@@ -15,191 +15,14 @@
 # https://cloud.google.com/vision/docs/quickstart-client-libraries
 # export GOOGLE_APPLICATION_CREDENTIALS="[PATH]"
 # export GOOGLE_APPLICATION_CREDENTIALS="/Users/lemberger/Documents/code/cloud/smarttag-2-5b02d5e85409.json"
+# google-auth
+# or use credentials=credentials (google.auth.credentials.Credentials) in client
+# https://google-auth.readthedocs.io/en/stable/user-guide.html#service-account-private-key-files
+# from google.oauth2 import service_account
+# credentials = service_account.Credentials.from_service_account_file('/path/to/key.json')
 
 # filename = '../artimage/multi-western/187124.jpg'
 # response.full_text_annotation.pages[0].blocks[0]
-# property {
-#   detected_languages {
-#     language_code: "sv"
-#   }
-# }
-# bounding_box {
-#   vertices {
-#     x: 215
-#     y: 22
-#   }
-#   vertices {
-#     x: 274
-#     y: 22
-#   }
-#   vertices {
-#     x: 274
-#     y: 41
-#   }
-#   vertices {
-#     x: 215
-#     y: 41
-#   }
-# }
-# paragraphs {
-#   property {
-#     detected_languages {
-#       language_code: "sv"
-#     }
-#   }
-#   bounding_box {
-#     vertices {
-#       x: 215
-#       y: 22
-#     }
-#     vertices {
-#       x: 274
-#       y: 22
-#     }
-#     vertices {
-#       x: 274
-#       y: 41
-#     }
-#     vertices {
-#       x: 215
-#       y: 41
-#     }
-#   }
-#   words {
-#     property {
-#       detected_languages {
-#         language_code: "sv"
-#       }
-#     }
-#     bounding_box {
-#       vertices {
-#         x: 215
-#         y: 22
-#       }
-#       vertices {
-#         x: 274
-#         y: 22
-#       }
-#       vertices {
-#         x: 274
-#         y: 41
-#       }
-#       vertices {
-#         x: 215
-#         y: 41
-#       }
-#     }
-#     symbols {
-#       property {
-#         detected_languages {
-#           language_code: "sv"
-#         }
-#       }
-#       bounding_box {
-#         vertices {
-#           x: 215
-#           y: 22
-#         }
-#         vertices {
-#           x: 229
-#           y: 22
-#         }
-#         vertices {
-#           x: 229
-#           y: 41
-#         }
-#         vertices {
-#           x: 215
-#           y: 41
-#         }
-#       }
-#       text: "H"
-#     }
-#     symbols {
-#       property {
-#         detected_languages {
-#           language_code: "sv"
-#         }
-#       }
-#       bounding_box {
-#         vertices {
-#           x: 232
-#           y: 27
-#         }
-#         vertices {
-#           x: 245
-#           y: 27
-#         }
-#         vertices {
-#           x: 245
-#           y: 41
-#         }
-#         vertices {
-#           x: 232
-#           y: 41
-#         }
-#       }
-#       text: "e"
-#     }
-#     symbols {
-#       property {
-#         detected_languages {
-#           language_code: "sv"
-#         }
-#       }
-#       bounding_box {
-#         vertices {
-#           x: 248
-#           y: 22
-#         }
-#         vertices {
-#           x: 260
-#           y: 22
-#         }
-#         vertices {
-#           x: 260
-#           y: 41
-#         }
-#         vertices {
-#           x: 248
-#           y: 41
-#         }
-#       }
-#       text: "L"
-#     }
-#     symbols {
-#       property {
-#         detected_languages {
-#           language_code: "sv"
-#         }
-#         detected_break {
-#           type: EOL_SURE_SPACE
-#         }
-#       }
-#       bounding_box {
-#         vertices {
-#           x: 262
-#           y: 27
-#         }
-#         vertices {
-#           x: 274
-#           y: 27
-#         }
-#         vertices {
-#           x: 274
-#           y: 41
-#         }
-#         vertices {
-#           x: 262
-#           y: 41
-#         }
-#       }
-#       text: "a"
-#     }
-#   }
-# }
-# block_type: TEXT
-
 # https://cloud.google.com/vision/docs/reference/rest/v1/images/annotate#TextAnnotation
 # >>> response.text_annotations[0]
 # locale: "sv"
@@ -292,32 +115,43 @@
 import io
 import os
 import torch
+from xml.etree.ElementTree  import XML, parse, tostring
 from nltk.metrics.distance import edit_distance
 import cv2 as cv
 from google.cloud import vision
 from google.cloud.vision import types
-from math import floor
-from common.utils import cd
+from google.oauth2 import service_account
+from math import floor, ceil
+from ..common.utils import cd
+from ..common.progress import progress
 from .. import config
 
-ALLOWED_FILE_EXTENSIONS =['txt', 'jpg']
+ALLOWED_FILE_EXTENSIONS =['xml', 'jpg']
 
 class OCR():
     """
+    [WIP] Will need to be included in encoder module?
     Extracts text elements from images and encode as features for relevant segments of the respective text examples.
+
+    Args:
+        path: path to the directory where xml documents and images are found
+        G: size of the grid, grid is G x G
+        T: T similarity threshold (edit distance or Levenshtein distance / word length) when searching for best match between OCRed element and text
+        accound_key: path to the account_key json file created in https://console.cloud.google.com/apis/credentials
     """
 
-    G = 5 # grid is G x G
-    T = 0.8 # similarity threshold when searching for best match between OCRed element and text
-
-    def __init__(self, path):
-        self.client = vision.ImageAnnotatorClient()
-        self.examples = self.load_examples()
+    def __init__(self, path, G=5, T=0.1, account_key='/Users/lemberger/Documents/code/cloud/smarttag-2-5b02d5e85409.json'):
+        self.path = path
+        self.G = G
+        self.T = T
+        self.client = vision.ImageAnnotatorClient(credentials = service_account.Credentials.from_service_account_file(account_key))
+        self.examples = self.load_from_dir()
         self.N = len(self.examples)
-        self.T = torch.Tensor(self.N, self.G, len(text))
+        self.context_codes = []
+        #self.context_tensor = torch.Tensor(self.N, self.G, len(text))
 
-    def load_from_dir(self, path):
-        xml_documents = self.import_files(path)
+    def load_from_dir(self):
+        xml_documents = self.import_xml_files()
         examples = self.load_examples(xml_documents)
         return examples
     
@@ -329,15 +163,16 @@ class OCR():
         encoded_examples = []
 
         for id in examples:
-            
+            example = examples[id]
+            text = ''.join([s for s in example.itertext()])
             if text:
-                example = examples[id]
                 img_filename = example.attrib['img']
                 #encoded_features, _, _ = XMLEncoder.encode(example)
+                #encoded_context = OCR.encode(example) #or somethign like that in the future 
                 example = {
                     'provenance': id,
                     'text': text,
-                    'img'; img_filename
+                    'img': img_filename
                 }
                 encoded_examples.append(example)
             else:
@@ -346,26 +181,27 @@ class OCR():
         return encoded_examples
 
 
-    def import_files(self, XPath_to_examples='.//sd-panel'):
+    def import_xml_files(self, XPath_to_examples='.//sd-panel'):
         """
-        Import xml documents from dir. In each document, extracts examples using XPath.
+        Import xml documents from path. In each document, extracts examples using XPath. 
         """
 
-        with cd(config.data_dir):
+        with cd(self.path):
             # path = os.path.join(self.compendium, subset)
-            print("\nloading from:", path)
-            os.
-            filenames = [f for f in os.listdir(path) if os.path.splitext(f)[-1] == 'xml']
+            print("\nloading from:", self.path)
+            filenames = os.listdir()
+            filenames = [f for f in filenames if f.split('.')[-1] == 'xml']
+            print("filenames", filenames)
             examples = {}
             for i, filename in enumerate(filenames):
                 try:
-                    with open(os.path.join(path, filename)) as f: 
+                    with open(filename) as f: 
                         xml = parse(f)
                     for j, e in enumerate(xml.findall(XPath_to_examples)):
                         id = filename + "-" + str(j) # unique id provided filename is unique (hence limiting to single allowed file extension)
                         examples[id] = e
                 except Exception as e:
-                    print("problem parsing", os.path.join(path, filename))
+                    print("problem parsing", filename)
                     print(e)
                 progress(i, len(filenames), "loaded {}".format(filename))
         return examples
@@ -373,34 +209,30 @@ class OCR():
     def get_example(self, i):
         text = self.examples[i]['text']
         img_filename = self.examples[i]['img']
-        with io.open(img_filename, 'rb') as image_file:
-            img = image_file.read()
-            img_cv = cv.read(image_file)
-            shape = img_cv.shape()
-        return text, img, shape
-    
+        with cd(self.path):
+            with io.open(img_filename, 'rb') as image_file:
+                img = image_file.read()
+            img_cv = cv.imread(img_filename) # hack, would be better not to read file twice... dunno how to get image size otherwise!!
+        shape = img_cv.shape
+        h = shape[0]
+        w = shape[1]
+        return text, img, h, w
+
     def get_ocr(self, img):
-
-        # Loads the image into memory
-
         image = types.Image(content=img)
-
-        #extract image dimensions HERE
-
-        # Performs text detection on the image file
-        # https://googleapis.github.io/google-cloud-python/latest/vision/gapic/v1/api.html#google.cloud.vision_v1.ImageAnnotatorClient.text_detection
-        # text_detection(image, max_results=None, retry=None, timeout=None, **kwargs)#
+        # Performs text detection on the image file; see https://googleapis.github.io/google-cloud-python/latest/vision/gapic/v1/api.html#google.cloud.vision_v1.ImageAnnotatorClient.text_detection
         response = self.client.text_detection(image)
         annotations = response.text_annotations
-        print('Labels:')
+        print('\nTextual elements detected on image:')
         for a in annotations:
             print(a.description)
+        print("###########")
         return annotations
 
-    def get_coordinates(self, annotation):
-        return annotations.bounding_poly.vertices[0]
+    def get_coordinates(self, annot):
+        return annot.bounding_poly.vertices[0].x, annot.bounding_poly.vertices[0].x
     
-    def get_orientation(self, annotation):
+    def get_orientation(self, annot):
         """
         when the text is horizontal it might look like:
         0----1
@@ -412,8 +244,8 @@ class OCR():
         1----0
         and the vertice order will still be (0, 1, 2, 3).
         """
-        x0 = annotation.bounding_poly.vertices[0].x0
-        x1 = annotation.bounding_poly.vertices[1].x
+        x0 = annot.bounding_poly.vertices[0].x
+        x1 = annot.bounding_poly.vertices[1].x
         if x1 > x0:
             return 'horizontal'
         elif x1 < x0:
@@ -421,43 +253,70 @@ class OCR():
         else:
             return 'unknown'
 
-    def get_text(self, element):
-        return annotation.description
+    def get_text(self, annot):
+        return annot.description
 
-    def grid_index(self, h, w, element):
-        """transforms coordinates into indexed grid position"""
-        x, y = self.get_coordinates(element)
+    def grid_index(self, h, w, annot):
+        """
+        Transforms coordinates into an indexed grid position.
+        Position indexed from top left to right bottom
+        
+        Args:
+            h, w : height and width of the image
+            annot: ocr annotation
+        """
+        x, y = self.get_coordinates(annot)
         grid_pos = (floor(self.G * (y / h)) * self.G) + floor(self.G * (x / w))
         return grid_pos
 
-    def best_matches(self, text, element):
-        query = self.get_text(element)
+    def best_matches(self, text, annot):
+        query = self.get_text(annot)
         l = len(query)
         L = len(text)
         matches = []
         for i in range(L-l):
             dist = edit_distance(text[i:i+l], query) # Levenshtein distance between query and text at this position
-            if dist <= T:
+            dist /= l # distance per word length
+            if dist <= self.T:
                 matches.append({
                     'match': text[i:i+l],
                     'length': l,
-                    'dist': dist,
+                    'score': 1-dist,
                     'pos': i
                 })
         return matches
 
+    def add_context_(self, context_tensor, pos_on_grid, pos_in_text, length, score):
+        context_tensor[pos_on_grid, pos_in_text:pos_in_text+length] = score
 
-    def add_context(self, pos_on_grid, pos_in_text, length, score):
-        self.T[pos_on_grid, pos_in_text:pos_in_text+length] = score
-
+    def encode_ocr(self, text, h, w, annotations):
+        context_tensor = torch.zeros(self.G ** 2, len(text))
+        for annot in annotations:
+            pos_on_grid = self.grid_index(h, w, annot)
+            matches = self.best_matches(text, annot)
+            for m in matches:
+                print(m)
+                self.add_context_(context_tensor, pos_on_grid, m['pos'], m['length'], m['score'])
+        return context_tensor
+    
     def run(self):
-        for i, filename in enumerate(self.filenames):
-            text, img, shape = self.get_example(i)
-            h = shape[0]
-            w = shape[1]
+        for i in range(self.N): # for text, img, shape in self.examples # define class Examples and class Example
+            text, img, h, w = self.get_example(i)
             annotations = self.get_ocr(img)
-            for a in annotations:
-                pos_on_grid = self.grid_index(h, w, a) # 
-                matches = self.best_matches(text, a)
-                for pos_in_text in matches:
-                    self.add_context(pos_on_grid, pos_in_text['pos'], pos_in_text['length'], pos_in_text['dist'])
+            encoded_ocr_context = self.encode_ocr(text, h, w, annotations)
+            self.context_codes.append(encoded_ocr_context)
+
+
+def main():
+    test_path = os.path.join(config.data_dir, 'test_img', 'train')
+    ocr = OCR(test_path)
+    ocr.run()
+    print(ocr.examples)
+    for i, t in enumerate(ocr.context_codes):
+        print(ocr.examples[i]['text'])
+        for j in range(t.size(0)):
+            print("".join([['-','+'][ceil(s)] for s in t[j]]))
+
+
+if __name__ == '__main__':
+    main()
