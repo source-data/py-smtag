1023ce2ac96c9a115544a2a9065668d48bfa7bc4
Alejandro Riera <ariera@gmail.com>
2018-12-11 17:42:36 +0100

    remove trailing white space

--- smtag/common/config.py
+++ smtag/common/config.py
@@ -30,7 +30,7 @@ class Config():
     _scans_dir_name    = "scans" # results of hyperparameter scans
     _img_grid_size     = 10 # grid size used to encode the location of elements on images
     _viz_cxt_features  = 576 # number of features used from pre-trained visual analysis network
-    _nbits             = 32 # number of features use to encode characters 
+    _nbits             = 32 # number of features use to encode characters
     _marking_char      = u'\uE000' # Substitution special xml-compatible character used to mark anonymized entities.
     _min_padding       = 20 # the number of (usually space) characters added to each example as padding to mitigate 'border effects' in learning
     _min_size          = 140 # input needs to be of minimal size to survive successive convergent convolutions; ideally, should be calculated analytically
