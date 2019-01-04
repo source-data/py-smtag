739a46152f667caaa352f242f993605c587e84d2
tlemberger <thomas.lemberger@gmail.com>
2019-01-04 19:25:40 +0100

    fixed bug in panelization to avoid empty panel legend when panel is start of the text

--- smtag/predict/serializer.py
+++ smtag/predict/serializer.py
@@ -159,7 +159,7 @@ class AbstractTagger(AbstractSerializer):
             segments = []
             if len(boundaries.nonzero()) != 0: # if example i has a position where boundaries[i] is 1, we need to segment
                 for b in boundaries: # what if multiple kind of boundaries? this would be too complicated for the moment!!
-                    if b in token_start_positions:
+                    if b in token_start_positions and b > 0: # ignore 'trivial' boundary if it is at the very start of the text
                         #find index of corresponding token
                         next_token_index = token_start_positions.index(b)
                         # add token from previous start to token before next
