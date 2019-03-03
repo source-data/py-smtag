bug fixing of lazy loading, scaling of context viz features

smtag/datagen/context.py
@@ -56,8 +56,6 @@ for m in vgg_urls:
 # m(x).size() # -->torch.Size([1, 2048, 2, 2])
 # x.numel() # --> 8192
 
-ALLOWED_FILE_EXTENSIONS =['.jpg', '.jpeg']
-
 class VisualContext(object):
 
     def __init__(self, path, selected_output_module=28):
@@ -112,16 +110,9 @@ class VisualContext(object):
             output = self.net(normalized)
         return output # 4D 1 x 512 x 14 x 14
 
-    def load_viz_context(self, graphic_filename):
-        basename = os.path.basename(graphic_filename)
-        basename = os.path.splitext(basename)[0]
-        viz_context_filename = os.path.join(self.path, basename, '.pyth')
-        viz_tensor = torch.load(viz_context_filename)
-        return viz_tensor
-
     def run(self):
         with cd(self.path):
-            filenames = [f for f in os.listdir() if os.path.splitext(f)[-1] in ALLOWED_FILE_EXTENSIONS]
+            filenames = [f for f in os.listdir() if os.path.splitext(f)[-1] in config.allowed_img]
             N = len(filenames)
             for i, filename in enumerate(filenames):
                 basename = os.path.splitext(filename)[0]

smtag/datagen/convert2th.py
@@ -27,7 +27,22 @@ from .brat import BratImport
 from .context import VisualContext, PCA_reducer
 from .. import config
 
-NBITS = config.nbits
+
+class EncodedExample:
+    features_filename = 'features.pyth'
+    text_filename = 'text.txt'
+    textcoded_filename = 'textcoded.pyth'
+    provenance_filename = 'provenance.txt'
+    ocr_context_filename = 'ocr_context.pyth'
+    viz_context_filename = 'viz_context.pyth'
+
+    def __init__(self, provenance, text, features, textcoded=None, ocr_context=None, viz_context=None):
+        self.provenance = provenance
+        self.text = text
+        self.features = features
+        self.textcoded = textcoded
+        self.ocr_context = ocr_context
+        self.viz_context = viz_context
 
 class Sampler():
 
@@ -89,16 +104,6 @@ class Sampler():
     #     text_min = min(stats)
     #     print("\nlength of the {} examples selected ({} skipped because too short):".format(N, skipped_examples))
     #     print("{} +/- {} (min = {}, max = {})".format(text_avg, text_std, text_min, text_max))
-
-class EncodedExample:
-    
-    def __init__(self, provenance, text, features, textcoded=None, ocr_context=None, viz_context=None):
-        self.provenance = provenance
-        self.text = text
-        self.features = features
-        self.textcoded = textcoded
-        self.ocr_context = ocr_context
-        self.viz_context = viz_context
 
 class Augment():
     """
@@ -164,6 +169,7 @@ class Augment():
             viz_context4th = None
             if encoded_example.viz_context is not None:
                 viz_context4th = self.pca.reduce(encoded_example.viz_context)
+                viz_context4th /= viz_context4th.max()
             #provenance, text, features, textcoded=None, ocr_context=None, viz_context=None
             processed_example = EncodedExample(encoded_example.provenance, padded_frag, features4th, textcoded4th, ocr_context4th, viz_context4th)
             self.save(path_to_encoded, j, processed_example)
@@ -171,22 +177,22 @@ class Augment():
                 Augment.display(padded_frag, features4th, ocr_context4th, viz_context4th)
 
 
-    def save(self, path, j_th_iteration, encoded_example):
+    def save(self, path:str, j_th_iteration:int, encoded_example:EncodedExample):
         full_path = path + "_" + str(j_th_iteration)
         if os.path.exists(full_path):
             print(f"skipping {full_path}: already exists!")
         else:
             os.mkdir(full_path)
-            with open(os.path.join(full_path, "provenance.txt"),'w') as f:
+            with open(os.path.join(full_path, encoded_example.provenance_filename),'w') as f:
                 f.write(encoded_example.provenance)
-            with open(os.path.join(full_path, "text.txt"), 'w') as f:
+            with open(os.path.join(full_path, encoded_example.text_filename), 'w') as f:
                 f.write(encoded_example.text)
-            torch.save(encoded_example.textcoded, os.path.join(full_path, "textcoded.pyth"))
-            torch.save(encoded_example.features, os.path.join(full_path, "features.pyth"))
+            torch.save(encoded_example.textcoded, os.path.join(full_path, encoded_example.textcoded_filename))
+            torch.save(encoded_example.features, os.path.join(full_path, encoded_example.features_filename))
             if encoded_example.ocr_context is not None:
-                torch.save(encoded_example.ocr_context, os.path.join(full_path, "ocr_context.pyth"))
+                torch.save(encoded_example.ocr_context, os.path.join(full_path, encoded_example.ocr_context_filename))
             if encoded_example.viz_context is not None:
-                torch.save(encoded_example.viz_context, os.path.join(full_path, "viz_context.pyth"))
+                torch.save(encoded_example.viz_context, os.path.join(full_path, encoded_example.viz_context_filename))
 
 
     @staticmethod