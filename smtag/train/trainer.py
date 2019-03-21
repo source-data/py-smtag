8e45291ce833d33dc75a03f9cdff53ec83082038
tlemberger <thomas.lemberger@gmail.com>
2019-03-21 18:12:11 +0100

    timeout 60sec

--- smtag/train/trainer.py
+++ smtag/train/trainer.py
@@ -54,8 +54,8 @@ class Trainer:
         self.batch_size = self.opt.minibatch_size
         self.trainset = trainset
         self.validation = validation
-        self.trainset_minibatches = DataLoader(trainset, batch_size=self.batch_size, shuffle=True, collate_fn=self.collate_fn, num_workers=self.num_workers, drop_last=True, timeout=10)
-        self.validation_minibatches = DataLoader(validation, batch_size=self.batch_size, shuffle=True, collate_fn=self.collate_fn, num_workers=self.num_workers, drop_last=True, timeout=10)
+        self.trainset_minibatches = DataLoader(trainset, batch_size=self.batch_size, shuffle=True, collate_fn=self.collate_fn, num_workers=self.num_workers, drop_last=True, timeout=60)
+        self.validation_minibatches = DataLoader(validation, batch_size=self.batch_size, shuffle=True, collate_fn=self.collate_fn, num_workers=self.num_workers, drop_last=True, timeout=60)
         self.evaluator = Accuracy(self.validation_minibatches, tokenize=False)
         self.console = Show('console')
 
