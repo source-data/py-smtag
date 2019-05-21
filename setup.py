1fd3529a1414837b6649ec947a1a4cdd4b3b3210
tlemberger <thomas.lemberger@gmail.com>
2019-05-21 08:49:03 +0100

    version 2.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
     name="smtag",
-    version="2.0.0",
+    version="2.0.1",
     python_requires='>=3.6',
     author="Source Data",
     author_email="source_data@embo.org",
