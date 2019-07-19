9f71067b039d55995683a2a515bf3749fa39ecd0
Thomas Lemberger <thomas.lemberger@embo.org>
2019-07-19 14:19:51 +0200

    harmonizing marking and masking char with vsearch

--- smtag/common/config/configuration.py
+++ smtag/common/config/configuration.py
@@ -52,8 +52,8 @@ class Config():
     _ocr_max_edit_dist = 0.2 # max edit distance per character length between ocr term and matching term in caption
     _ocr_min_overlap   = 3 # minimum lenght of overlap between ocr term and caption term
     _nbits             = 8 # 17 # number of features use to encode characters; 31 for full unicode, 17 for emoji and greek; 7 for ASCII
-    _marking_char      = u'\uE000' # Substitution special xml-compatible character used to mark anonymized entities.
-    _padding_char      = " " # character used to padd strings; would be smarter to use character different from space
+    _marking_char      = '_' #u'\uE000' # Substitution special xml-compatible character used to mark anonymized entities.
+    _padding_char      = '`' # " " # character used to padd strings; would be smarter to use character different from space
     _min_padding       = 100 # the number of (usually space) characters added to each example as padding to mitigate 'border effects' in learning
     _min_size          = 380 # input needs to be of minimal size to survive successive convergent convolutions with unet2 with 3 super layers and no padding; ideally, should be calculated analytically
     _default_threshold = 0.5 # threshold applied by default when descritizing predicted value and when considering a predicted value a 'hit' in accuracy calculation
