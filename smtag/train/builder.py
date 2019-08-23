07a167a6718ad7ca19368ab40de2e199074d7268
Thomas Lemberger <thomas.lemberger@embo.org>
2019-08-24 00:11:26 +0200

    trying to compress CatStack

--- smtag/train/builder.py
+++ smtag/train/builder.py
@@ -94,7 +94,9 @@ class CatStackWithVizContext(nn.Module):
             )
             self.blocks.append(block)
             in_channels = out_channels
-        self.out_channels = cumul_channels
+        # self.out_channels = cumul_channels
+        self.compress = nn.Conv1d(cumul_channels, 128, 1, 1)
+        self.out_channels = 128
 
     def forward(self, x: torch.Tensor, context_list) -> torch.Tensor:
 
@@ -108,4 +110,5 @@ class CatStackWithVizContext(nn.Module):
             x = self.blocks[i](x)
             x_list.append(x.clone())
         y = torch.cat(x_list, 1)
+        y = self.compress(y)
         return y
