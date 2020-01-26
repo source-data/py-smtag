6ba6b1e3a07d89af56b0d61e2da2dd7684ec2e8c
Thomas Lemberger <thomas.lemberger@embo.org>
2020-01-26 11:15:02 +0100

    trying the traditional way with .to(device)

--- smtag/common/embeddings.py
+++ smtag/common/embeddings.py
@@ -21,9 +21,11 @@ if config.embeddings_model:
     embedding_model = load_model(config.embeddings_model, config.embeddings_dir, CatStack)
     if torch.cuda.is_available():
         print(f"{torch.cuda.device_count()} GPUs available for embeddings.")
-        embedding_model = embedding_model.cuda()
+        device = torch.device("cuda:0")
+        # embedding_model = embedding_model.cuda()
         gpu_model = nn.DataParallel(embedding_model)
         gpu_model.hp = embedding_model.hp # not sure if necessary
+        gpu_model.to(device)
         embedding_model = gpu_model
 else:
     embedding_model = None
